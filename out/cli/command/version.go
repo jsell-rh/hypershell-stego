7db2c4624bc9143457b7617f335805051300d65e
John Sell <jsell@redhat.com>
2026-09-16 09:31:34 -0400

    build: adopt prepared migration correction

--- out/cli/command/version.go
+++ out/cli/command/version.go
@@ -13,7 +13,7 @@ import (
 func CompilerBuild() buildidentity.Record {
 	var record buildidentity.Record
 	// The compiler supplies this JSON value from a typed record during generation.
-	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260916132505-20d6e2ee8f9a\",\"vcs\":\"git\",\"revision\":\"20d6e2ee8f9a7074d26a385804f0490527f0710c\",\"source_state\":\"clean\"}"), &record); err != nil {
+	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260916133021-3a6e00e15c28\",\"vcs\":\"git\",\"revision\":\"3a6e00e15c28593048ed6bde5e3346762229a1d4\",\"source_state\":\"clean\"}"), &record); err != nil {
 		panic("invalid generated compiler build record")
 	}
 	return record
