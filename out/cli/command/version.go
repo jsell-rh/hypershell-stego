7ee6fa930529fd0e68a35ea4137d83d4135c5179
John Sell <jsell@redhat.com>
2026-09-16 09:27:02 -0400

    build: adopt bounded state key and common scan sequence runtime

--- out/cli/command/version.go
+++ out/cli/command/version.go
@@ -13,7 +13,7 @@ import (
 func CompilerBuild() buildidentity.Record {
 	var record buildidentity.Record
 	// The compiler supplies this JSON value from a typed record during generation.
-	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260916121958-e01e624f26c5\",\"vcs\":\"git\",\"revision\":\"e01e624f26c58e9ff33e7dc1422b540cb70381ee\",\"source_state\":\"clean\"}"), &record); err != nil {
+	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260916132505-20d6e2ee8f9a\",\"vcs\":\"git\",\"revision\":\"20d6e2ee8f9a7074d26a385804f0490527f0710c\",\"source_state\":\"clean\"}"), &record); err != nil {
 		panic("invalid generated compiler build record")
 	}
 	return record
