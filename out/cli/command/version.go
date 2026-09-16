cc81124416e361e14957e81be66b944c21d2fc58
John Sell <jsell@redhat.com>
2026-09-16 12:29:44 -0400

    Regenerate with the verified compiler build identity

--- out/cli/command/version.go
+++ out/cli/command/version.go
@@ -13,7 +13,7 @@ import (
 func CompilerBuild() buildidentity.Record {
 	var record buildidentity.Record
 	// The compiler supplies this JSON value from a typed record during generation.
-	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"(devel)\",\"vcs\":\"unknown\",\"revision\":\"unknown\",\"source_state\":\"unknown\"}"), &record); err != nil {
+	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260916161126-25cb9b2d02e1\",\"vcs\":\"git\",\"revision\":\"25cb9b2d02e1562a1cf36923e52116be0abf71ae\",\"source_state\":\"clean\"}"), &record); err != nil {
 		panic("invalid generated compiler build record")
 	}
 	return record
