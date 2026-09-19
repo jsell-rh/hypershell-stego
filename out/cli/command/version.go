f8942dbfc35678ffab1f1a8882840aaf80ac8e09
John Sell <jsell@redhat.com>
2026-09-19 18:55:32 -0400

    build: regenerate bounded controller retries from verified compiler

--- out/cli/command/version.go
+++ out/cli/command/version.go
@@ -13,7 +13,7 @@ import (
 func CompilerBuild() buildidentity.Record {
 	var record buildidentity.Record
 	// The compiler supplies this JSON value from a typed record during generation.
-	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260919211501-3220812f5643\",\"vcs\":\"git\",\"revision\":\"3220812f56430b81826f9505a6f88d3074fd241d\",\"source_state\":\"clean\"}"), &record); err != nil {
+	if err := json.Unmarshal([]byte("{\"go_version\":\"go1.26.8\",\"goos\":\"linux\",\"goarch\":\"amd64\",\"module_version\":\"v0.0.0-20260919224439-ee348b819bb4\",\"vcs\":\"git\",\"revision\":\"ee348b819bb43a738a6aad2fc14cb76ce3b1ec21\",\"source_state\":\"clean\"}"), &record); err != nil {
 		panic("invalid generated compiler build record")
 	}
 	return record
